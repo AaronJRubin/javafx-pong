a6ecf9b163e0919e8a5f78352a888e32e5cee67e
Aaron J Rubin <aaronjrubin1@gmail.com>
2014-10-27 13:44:30 +0900

    Added comments describing use of DistanceBall

--- src/main/java/game/DistanceBall.java
+++ src/main/java/game/DistanceBall.java
@@ -2,11 +2,22 @@ package main.java.game;
 
 import java.text.DecimalFormat;
 
-
+/** 
+This class is constructed with a ball and a "reference point," corresponding to a horizontal coordinate.
+In practice, this horizontal coordinate will be the position of a paddle controlled by an AI.
+Taking into account the horizontal and vertical velocities of the ball, the constructor
+computes the expected vertical height of the ball when the ball reaches that horizontal coordinate,
+as well as the number of move actions needed to reach that coordinate, where a move action consists of offsetting
+the ball's position by the x and y velocities. This class is used in the AI's optimal move algorithm,
+and nowhere else.
+*/
 class DistanceBall implements Comparable<DistanceBall> {
 	
+	// Used for toString, which I found useful when debugging this class.
 	private final static DecimalFormat df = new DecimalFormat("#.###");
+	// The number of movies needed for the ball to reach the reference point passed in.
 	private final int numberMoves;
+	// The expected y-coordinate of the ball when it reaches the reference point passed in.
 	private final double adjustedHeight;
 	
 	public DistanceBall(Ball b, double referencePoint) {
@@ -50,6 +61,12 @@ class DistanceBall implements Comparable<DistanceBall> {
 		return adjustedHeight;
 	}
 
+	/** 
+	It is useful to sort DistanceBalls by number of moves when computing the AI's optimal action.
+	The paddle AI looks at the closest balls (the ones with the least number of moves needed to reach it)
+	and adjusts its position accordingly so as to be guaranteed to hit the closest ball, as close as possible
+	to the next-closest given the need to hit the first-closest, etc.
+	*/
 	@Override
 	public int compareTo(DistanceBall o) {
 		return numberMoves - o.getNumberMoves();
